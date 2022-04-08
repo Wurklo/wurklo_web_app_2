a661ea9b89e1d2150838ab5ea4bde68ed1baf763
Bobby Keel <bobbykeel76@gmail.com>
2022-04-08 01:38:07 -0500

    styling my account

--- src/components/ProfileInfo.js
+++ src/components/ProfileInfo.js
@@ -8,17 +8,18 @@ function ProfileInfo({name, skill, imageUrl, availability, phone, email, portfol
             <Col lg={3} className="profileInfo__image">
                 <img src={imageUrl} className="shadow" alt={name} />
             </Col>
-            <Col md={3} className='mt-4 mt-md-2'>
+            <Col md={3} className='profileInfo__info mx-auto m-0 ms-md-3 mt-4 mt-md-0 py-3 p-md-3 shadow'>
                 <h3>{name}</h3>
                 <p>{skill}</p>
-                <p>$25/hr</p>
+                <p>Rate: $25/hr</p>
                 <p>Available {availability}</p>
                 <p>P# {phone}</p>
                 <p>E# {email}</p>
                 <p>Portfolio {portfolioLink}</p>
             </Col>
-            <Col md={6}>
+            <Col md={5} className="profileInfo__performanceEval mx-auto ms-md-5 mt-4 mt-md-0 p-3 text-center shadow">
                 <h2>Performance Evaluation</h2>
+                <p>Coming soon</p>
             </Col>
         </>
     )
