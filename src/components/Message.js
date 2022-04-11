660f07e771af89b1b149312b2443f33ebee4b70e
Bobby Keel <bobbykeel76@gmail.com>
2022-04-11 11:01:12 -0500

    time and scrollable contained messagebox

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,13 +1,11 @@
+import moment from 'moment';
 import React, { forwardRef } from 'react'
 
 const Message = forwardRef(({ username, message }, ref) => {
   const isUser = username === message.username;
-  console.log("username:", username)
-  console.log("message username:", message.username)
-  console.log("user:", isUser)
 
   return (
-    <div ref={ref} className={'p-2 text-white mb-2 ' + (isUser ? 'message__isUser bg-primary' : 'message__isNotUser bg-secondary text-black')}>{message.username}: {message.message}</div>
+    <div ref={ref} className={'p-2 text-white mb-2 ' + (isUser ? 'message__isUser bg-primary' : 'message__isNotUser bg-secondary text-black')}>{message.username}: {message.message} {moment(message.timestamp.seconds * 1000).format('MMMM Do YYYY, h:mm:ss a')}</div>
   )
 })
 
