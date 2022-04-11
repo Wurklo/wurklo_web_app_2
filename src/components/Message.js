63c6490176c9872df046b14b22db2513322a7dc4
Bobby Keel <bobbykeel76@gmail.com>
2022-04-11 12:53:01 -0500

    message time done, message looks good now

--- src/components/Message.js
+++ src/components/Message.js
@@ -6,7 +6,11 @@ const Message = forwardRef(({ username, message }, ref) => {
 
   return (
     <div className={'d-flex ' + (isUser ? 'justify-content-start' : 'justify-content-end')}>
-      <div ref={ref} className={'p-2 text-white m-2 ' + (isUser ? 'message__isUser bg-primary shadow-sm' : 'message__isNotUser bg-secondary shadow-sm')}>{message?.username}: {message?.message} {moment(message?.timestamp?.seconds * 1000).format('MMMM Do YYYY, h:mm:ss a')}</div>
+      <div ref={ref} className={'p-2 text-white m-2 ' + (isUser ? 'message__isUser bg-primary shadow-sm' : 'message__isNotUser bg-secondary shadow-sm')}>{message?.username}: {message?.message} 
+      <div className='message__time'>
+        {moment(message?.timestamp?.seconds * 1000).format('MMMM Do YYYY, h:mm:ss a')}
+      </div>
+      </div>
     </div>
 
   )
