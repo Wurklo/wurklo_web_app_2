dad0f8eaeeaf74e646b2c6288df83622960b9a0d
Bobby Keel <bobbykeel76@gmail.com>
2022-04-10 23:03:15 -0500

    chatbox button

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -1,19 +1,37 @@
-import React, { useState } from 'react'
-import { Col, Input } from 'reactstrap'
+import React, { useEffect, useState } from 'react'
+import { Button, Col, Input } from 'reactstrap'
 import Message from './Message'
 
 function ChatBox() {
+    const [messages, setMessages] = useState([
+        {username: 'Bobby', text: 'Hey Guys!'},
+        {username: 'Nikki', text: 'Hey Ladies!'}
+    ]);
     const [message, setMessage] = useState('');
+    const [username, setUsername] = useState('');
+
+    useEffect(() => {
+
+    }, [])
+
+    useEffect(() => {
+        // setUsername(prompt('Please enter you name'))
+    }, [])
+
+    const handleSendMessage = () => {
+        console.log(message)
+    }
 
     return (
         <>
-            <Col className='mt-4'>
+            <Col className='chatBox mt-4'>
                 <Input
                     className='profile__messageInput mt-3 p-3 shadow'
                     placeholder='Send a message ...'
                     value={message}
                     onChange={e => setMessage(e.target.value)}
                 />
+                <Button color='primary' outline className='chatBox__button' onClick={handleSendMessage}>Send</Button>
                 <div className='profile__messageBox mb-5 rounded-bottom shadow'>
                     <Message />
                 </div>
