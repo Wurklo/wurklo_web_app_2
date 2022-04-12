733c1ee717fc1657242a78d4c7a467de904fc6fb
Bobby Keel <bobbykeel76@gmail.com>
2022-04-11 22:15:24 -0500

    login modal signin buttons

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -1,14 +1,13 @@
 import React, { useState } from 'react';
 import { Button, Col, Input, Modal, ModalBody, ModalFooter, ModalHeader, Progress } from 'reactstrap';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGoogle, faApple, faMicrosoft, faYahoo, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function LoginModal() {
     const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
 
     return (
         <>
-            <Button outline className='header__joinNowButton shadow-none border-0'
-                onClick={() => setIsLoginModalOpen(isLoginModalOpen ? false : true)}
-            >Join Now</Button>
             <Button color='primary' outline className=' shadow-none make-round'
                 onClick={() => setIsLoginModalOpen(isLoginModalOpen ? false : true)}
             >Sign in</Button>
@@ -25,20 +24,44 @@ function LoginModal() {
                     <Button
                         outline
                         color='danger'
-                        className='createWurker__button p-2 px-4 mt-0'
+                        className='googleSignin__button make-round p-2 px-4 mt-0'
                     >
-                        Login with Google
+                        Sign in with Google
+                        <FontAwesomeIcon icon={faGoogle} className="fs-5 ms-2" />
                     </Button>
                     <Button
                         outline
                         color='secondary'
-                        className='createWurker__button p-2 px-4 mt-3'
+                        className='appleSignin__button make-round p-2 px-4 mt-3'
                     >
-                        Login with Apple
+                        Sign in with Apple
+                        <FontAwesomeIcon icon={faApple} className="fs-5 ms-2" />
+                    </Button>
+                    <Button
+                        outline
+                        color='primary'
+                        className='microsoftSignin__button make-round p-2 px-4 mt-3'
+                    >
+                        Sign in with Microsoft
+                        <FontAwesomeIcon icon={faMicrosoft} className="fs-5 ms-2" />
+                    </Button>
+                    <Button
+                        outline
+                        color='danger'
+                        className='yahooSignin__button make-round p-2 px-4 mt-3'
+                    >
+                        Sign in with Yahoo
+                        <FontAwesomeIcon icon={faYahoo} className="fs-5 ms-2" />
+                    </Button>
+                    <Button
+                        outline
+                        color='dark'
+                        className='githubSignin__button make-round p-2 px-4 my-3'
+                    >
+                        Sign in with Github
+                        <FontAwesomeIcon icon={faGithub} className="fs-5 ms-2" />
                     </Button>
                 </ModalBody>
-                <ModalFooter className='d-inline py-1 text-center'>
-                </ModalFooter>
             </Modal>
         </>
     )
